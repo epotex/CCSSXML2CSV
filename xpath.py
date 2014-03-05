500da9f534d981a347c8c09c5786a7a499e90331
root <root@cvpn01.(none)>
2014-03-05 22:15:58 +0000

    xpath test

---
+++ xpath.py
@@ -0,0 +1,44 @@
+from lxml import etree
+file = 'xml-in.xml'
+#namespaces={'skos':'http://www.w3.org/2004/02/skos/core#','rdfs':'http://www.w3.org/2000/01/rdf-schema#','gemq':'http://purl.org/gem/qualifiers/','loc':'http://www.loc.gov/loc.terms/relators/','owl':'http://www.w3.org/2002/07/owl#','asn':'http://purl.org/ASN/schema/core/', 'cc':'http://creativecommons.org/ns#','dc':'http://purl.org/dc/elements/1.1/','dcterms':'http://purl.org/dc/terms/','foaf':'http://xmlns.com/foaf/0.1/', 'rdf':'http://www.w3.org/2000/01/rdf-schema#', 'dcterms': 'http://purl.org/dc/terms/'})
+#educationLevel = element.xpath('*[local-name()="educationLevel"]')
+
+
+tree = etree.parse(file).getroot()
+
+
+#Global Vars
+grade = "12'}"
+#gradefilter  = "{'{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource': 'http://purl.org/ASN/scheme/ASNEducationLevel/" + grade
+   
+
+for element in tree.getchildren():
+    #Xpath XML items Vars:
+    Statement = element.xpath('asn:Statement',  namespaces = tree.nsmap)
+    EducationLevel = element.xpath('//dcterms:educationLevel',  namespaces = tree.nsmap)
+    Subject = element.xpath('//dcterms:subject ',  namespaces = tree.nsmap)
+    Description = element.xpath('//dcterms:description ',  namespaces = tree.nsmap)
+    Haschild = element.xpath('//gemq:hasChild  ',  namespaces = tree.nsmap)
+    Language = element.xpath('//dcterms:language  ',  namespaces = tree.nsmap)
+    IsChildOf = element.xpath('//gemq:isChildOf ',  namespaces = tree.nsmap)
+    AuthorityStatus = element.xpath('//asn:authorityStatus ',  namespaces = tree.nsmap)
+    IndexingStatus = element.xpath('//asn:indexingStatus ',  namespaces = tree.nsmap)
+    IsPartOf = element.xpath('//dcterms:isPartOf ',  namespaces = tree.nsmap)
+
+    for a in element.xpath('//dcterms:educationLevel',  namespaces = tree.nsmap):
+        #print a.attrib
+        #print gradefilter
+        if  a.attrib == {'{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource': 'http://purl.org/ASN/scheme/ASNEducationLevel/10'}:
+            print Statement, EducationLevel
+        #else:
+         #   print "no"
+    
+    
+    
+
+
+   # if element.attrib == greadefilter:
+        
+    
+        
+
