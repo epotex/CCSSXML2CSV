d3e4a38e7f1d21a212c681a5b8b8cd32a8d91102
epotex <epotex@nana.co.il>
2014-03-05 23:09:51 -0500

    Parser improvements

--- xpath.py
+++ xpath.py
@@ -15,21 +15,26 @@ grade = "12'}"
 for element in tree.getchildren():
     #Xpath XML items Vars:
     Statement = element.xpath('asn:Statement',  namespaces = tree.nsmap)
-    EducationLevel = element.xpath('//dcterms:educationLevel',  namespaces = tree.nsmap)
-    Subject = element.xpath('//dcterms:subject ',  namespaces = tree.nsmap)
-    Description = element.xpath('//dcterms:description ',  namespaces = tree.nsmap)
-    Haschild = element.xpath('//gemq:hasChild  ',  namespaces = tree.nsmap)
-    Language = element.xpath('//dcterms:language  ',  namespaces = tree.nsmap)
-    IsChildOf = element.xpath('//gemq:isChildOf ',  namespaces = tree.nsmap)
-    AuthorityStatus = element.xpath('//asn:authorityStatus ',  namespaces = tree.nsmap)
-    IndexingStatus = element.xpath('//asn:indexingStatus ',  namespaces = tree.nsmap)
-    IsPartOf = element.xpath('//dcterms:isPartOf ',  namespaces = tree.nsmap)
+    EducationLevel = element.xpath('.//dcterms:educationLevel',  namespaces = tree.nsmap)
+    Subject = element.xpath('.//dcterms:subject ',  namespaces = tree.nsmap)
+    Description = element.xpath('.//dcterms:description ',  namespaces = tree.nsmap)
+    Haschild = element.xpath('.//gemq:hasChild  ',  namespaces = tree.nsmap)
+    Language = element.xpath('.//dcterms:language  ',  namespaces = tree.nsmap)
+    IsChildOf = element.xpath('.//gemq:isChildOf ',  namespaces = tree.nsmap)
+    AuthorityStatus = element.xpath('.//asn:authorityStatus ',  namespaces = tree.nsmap)
+    IndexingStatus = element.xpath('.//asn:indexingStatus ',  namespaces = tree.nsmap)
+    IsPartOf = element.xpath('.//dcterms:isPartOf ',  namespaces = tree.nsmap)
+    StatementNotation = element.xpath('asn:statementNotation ',  namespaces = tree.nsmap)
 
-    for a in element.xpath('//dcterms:educationLevel',  namespaces = tree.nsmap):
+  
+
+    for a in element.xpath('.//dcterms:educationLevel',  namespaces = tree.nsmap):
         #print a.attrib
         #print gradefilter
+     
         if  a.attrib == {'{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource': 'http://purl.org/ASN/scheme/ASNEducationLevel/10'}:
-            print Statement, EducationLevel
+         
+            print Subject[0].attrib, Description[0].text, Language[0].attrib
         #else:
          #   print "no"
     
