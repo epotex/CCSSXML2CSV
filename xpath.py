bac87fe1c49cf472a5fd25bb732dc318924c8026
unknown <moshe_edri@.mhe.mhc>
2014-03-06 18:25:03 -0500

    add Extract Value functions

--- xpath.py
+++ xpath.py
@@ -3,6 +3,12 @@ file = 'xml-in.xml'
 #namespaces={'skos':'http://www.w3.org/2004/02/skos/core#','rdfs':'http://www.w3.org/2000/01/rdf-schema#','gemq':'http://purl.org/gem/qualifiers/','loc':'http://www.loc.gov/loc.terms/relators/','owl':'http://www.w3.org/2002/07/owl#','asn':'http://purl.org/ASN/schema/core/', 'cc':'http://creativecommons.org/ns#','dc':'http://purl.org/dc/elements/1.1/','dcterms':'http://purl.org/dc/terms/','foaf':'http://xmlns.com/foaf/0.1/', 'rdf':'http://www.w3.org/2000/01/rdf-schema#', 'dcterms': 'http://purl.org/dc/terms/'})
 #educationLevel = element.xpath('*[local-name()="educationLevel"]')
 tree = etree.parse(file).getroot()
+def get_xpath_attrib_value_list(x):
+    for a in x:
+        dict = a.attrib
+        for value in dict.itervalues():
+            yield value
+
 #Global Vars
 grade = "12'}"
 Country ='USA'
@@ -15,27 +21,45 @@ Grade_Level = '5'
 GeneralDescription =  tree.findall('.//dcterms:description', namespaces = tree.nsmap)[0].text
 GeneralId =  tree.findall('.//asn:jurisdiction', namespaces = tree.nsmap)[0].attrib
 GeneralName =  tree.findall('.//dc:title', namespaces = tree.nsmap)[0].text
+EducationLevel =  tree.findall('.//dcterms:educationLevel', namespaces = tree.nsmap) #list of dict
+Statement =  tree.findall('.//asn:Statement', namespaces = tree.nsmap) #list of dict
+Subject =  tree.findall('.//dcterms:subject', namespaces = tree.nsmap) #list of dict
+Description =  tree.findall('.//dcterms:description', namespaces = tree.nsmap) #TEXT
+Comment = tree.findall('.//asn:comment',  namespaces = tree.nsmap)#TEXT
+Haschild = tree.findall('.//gemq:hasChild',  namespaces = tree.nsmap)
+IsChildOf = tree.findall('.//gemq:isChildOf',  namespaces = tree.nsmap)
+IsPartOf = tree.findall('.//dcterms:isPartOf',  namespaces = tree.nsmap)
+StatementNotation = tree.findall('.//asn:statementNotation',  namespaces = tree.nsmap)#TEXT
+
+    
+
+#for x in get_xpath_attrib_value_list(StatementNotation):
+ #   print x
 
 
+#Get the Header for the CSV file
 def get_csv_header():
     print 'Country', ';',    'State', ';',    'Standard Package', ';',    'Discipline', ';',    'Grade Level', ';',    'Ped Id', ';',    'Parent Id', ';',    'Selectable', ';',    'Name', ';',    'Description'
-
-def get_xpath_attrib_value(x):
+#Get Value from a list of dict
+#Get Value from a dict
+def get_xpath_attrib_value_dict(x):
     for key, value in x.iteritems():
         print  value
 
-def get_general_entety():
+#Get the General entity
+def get_general_entity():
     #Xpath XML items Vars:
     pass
     #print Country, ';',    State, ';',    Standard_Pack, ';',    Discipline, ';',    Grade_Level, ';',    GeneralId, ';',    '', ';',    'Selectable', ';',    GeneralName, ';',    GeneralDescription
-    
-def get_all_entetys():
+#Get the ALL entity(beside the General entity)
+def get_all_entitys():
     #Xpath XML items Vars:
     for element in tree.getchildren():
         for a in element.xpath('.//dcterms:educationLevel',  namespaces = tree.nsmap):
             Statement = element.xpath('asn:Statement',  namespaces = tree.nsmap)
-            EducationLevel = element.xpath('.//dcterms:educationLevel',  namespaces = tree.nsmap)
-            Subject = element.xpath('.//dcterms:subject ',  namespaces = tree.nsmap)
+            #EducationLevel = element.xpath('.//dcterms:educationLevel',  namespaces = tree.nsmap) #dict
+            edu =  tree.findall('.//dcterms:educationLevel', namespaces = tree.nsmap)#list of dict
+            #Subject = element.xpath('.//dcterms:subject ',  namespaces = tree.nsmap)
             Description = element.xpath('.//dcterms:description ',  namespaces = tree.nsmap)
             Comment = element.xpath('.//asn:comment ',  namespaces = tree.nsmap)
             Haschild = element.xpath('.//gemq:hasChild  ',  namespaces = tree.nsmap)
@@ -49,23 +73,12 @@ def get_all_entetys():
             if a.attrib == {'{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource': 'http://purl.org/ASN/scheme/ASNEducationLevel/8'}:
                for a in StatementNotation:
                    if  a.text is not None:
-                       yield Country, ';', State, ';', Standard_Pack, ';', Subject[0].attrib, ';', EducationLevel[0].attrib, ';', a.text, ';', IsChildOf[0].attrib, ';', selectable, ';', Description[0].text, ';', Comment
- 
-def test():
-     edu =  tree.findall('.//dcterms:educationLevel', namespaces = tree.nsmap)
-     for a in edu:
-        get_xpath_attrib_value(a)
-        
-     #get_xpath_attrib_value(edu)
-     
+                       yield Country, ';', State, ';', Standard_Pack, ';', get_xpath_attrib_value_list(Subject), ';', EducationLevel[0].attrib, ';', a.text, ';', IsChildOf[0].attrib, ';', selectable, ';', Description[0].text, ';', Comment
 
 
-
-
-test()
 #Script:#            
 #get_csv_header()
-#get_general_entety()    
+#get_general_entity()    
     
-#for a in get_all_entetys():
-#    print a
+for a in get_all_entitys():
+    print a
