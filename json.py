6de3860c48f426dfb0c256c5c661116636721fc7
epotex <epotex@nana.co.il>
2014-03-11 00:03:47 -0400

    prog

--- json.py
+++ json.py
@@ -4,40 +4,64 @@ import simplejson
 
 mathurl ="http://s3.amazonaws.com/asnstaticd2l/data/manifest/D10003FB.json"
 #Global Vars:
-
-
 #Lists:
-
-
-
 if __name__ == '__main__':
 
     req = urllib2.Request(mathurl)
     opener = urllib2.build_opener()
     data = opener.open(req)
     json = simplejson.load(data)
+    
    # ID = item.get('id')
     #asn_identifier = item.get('asn_identifier')
-    for item in json:
-        ID = item.get('id')
-        Subject = item.get('dcterms_subject').get('prefLabel')
-        Grades = item.get('dcterms_educationLevel')
-        Description = item.get('dcterms_description').get('literal')
-        Children = item.get('children')
-        #print Children.get('dcterms_description')
-        
-        #if  item.get('dcterms_educationalLevel') is not None:
-       # print item.get('dcterms_educationLevel')
-        
-def get_entity_grades_list(x):    
+#     for item in json:
+#         ID = item.get('id')
+#         Subject = item.get('dcterms_subject').get('prefLabel')
+#         Grades = item.get('dcterms_educationLevel')
+#         Description = item.get('dcterms_description').get('literal')
+#         Children = item.get('children')
+#         print json[2]
+for item in json:
+    print item.get('asn_identifier')
+               
+def get_entity_grades_list(x):
+    gradelevel='10'
     for item in x:
         grade_levels = []
         for grade in Grades:
            grade_levels.append(grade['prefLabel'])
-    print grade_levels
-              
-get_entity_grades_list(Children)    
-for child in  Children:
-    for entity in child:
-           
-         
+           #print grade_levels
+    if gradelevel in grade_levels:
+        print 'Gradelevel',gradelevel, 'found!'
+    else:
+        print 'Gradelevel',gradelevel,'Not found!'    
+     
+#get_entity_grades_list(Children)    
+
+# for child in  Children:   
+#     get_entity_grades_list(child) 
+# 
+# for item in json:
+#     print item['asn_statementLabel']
+#         print x.get('asn_statementLabel')    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
--- moshe.py
+++ moshe.py
@@ -1,38 +1,56 @@
 from lxml import etree
+
 file = 'xml-in.xml'
 tree = etree.parse(file).getroot()
-
-all = tree.xpath('//asn:Statement[@*]', namespaces = tree.nsmap)
+all = tree.getchildren()#xpath('//asn:Statement[@*]', namespaces = tree.nsmap)
 
 #===============================================================================
 # Finctions
 #===============================================================================
-def get_statment():    
-     if StatementNotation == []:
-         pass
-     else:
-         yield StatementNotation
+for x in all:
+    UrlId = x.xpath('//asn:Statement[@rdf:about]',namespaces = tree.nsmap)
+    Grade = x.xpath('.//dcterms:educationLevel [@rdf:resource]',  namespaces = tree.nsmap)
+    StatementNotation = x.xpath('./asn:statementNotation/text()',  namespaces = tree.nsmap)
+    Description = x.xpath('//asn:Statement/dcterms:description/text()',  namespaces = tree.nsmap)
 
-def get_edu_level():
-    for a in EDU:
-        yield a
-
+def get_statement():
+    for item in StatementNotation:
+        if item == []:
+            pass
+        else:
+            yield item
+def get_description():
+    for item in Description:
+        if item == []:
+            pass
+        else:
+            yield item    
+def get_grade_level():
+    for item in Grade:
+        if item == []:
+            pass
+        else:
+            yield item.values()
+def get_UrlId():
+    for item in UrlId:
+        if item == []:
+            pass
+        else:
+            yield item.values()
 #===============================================================================
 #Script 
 #===============================================================================
-
-for x in all:
-    s=''
-    URLID = x.xpath('//asn:Statement[@rdf:about]',namespaces = tree.nsmap)
-    EDU = x.xpath('.//dcterms:educationLevel [@rdf:resource]',  namespaces = tree.nsmap)
-    StatementNotation = x.xpath('./asn:statementNotation/text()',  namespaces = tree.nsmap)
-    for s in get_statment():
+for element in all:
+    for state in get_statement():
         pass
-    for e in get_edu_level():
+    for level in get_grade_level():
         pass
-    for ID in URLID:
+    for ID in get_UrlId():
         pass
-            
+    for x in get_description():
+        pass        
+    print ID,level,state,x
+    
+    
 
-
-print  ID.attrib, e.attrib, s   
+               
