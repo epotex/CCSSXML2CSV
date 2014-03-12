dedf702b90d0234de16f6a97d76ad29bb5f2f6b6
unknown <moshe_edri@.mhe.mhc>
2014-03-12 14:37:48 -0400

    missing keys

--- json.py
+++ json.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python
-
 import sys
 import urllib2
 import simplejson
 
-
 URL ="https://s3.amazonaws.com/asnstaticd2l/data/manifest/D10003FB.json"
 EDU_LABEL = 'dcterms_educationLevel'
 PREF_LABEL = 'prefLabel'
+gradefilter = '6'
 
 def get_doc():
     req = urllib2.Request(URL)
     opener = urllib2.build_opener()
     data = opener.open(req)
     return simplejson.load(data)
-
-def select_entities(doc, pref_value):
-    entities = []
-    for entity in doc:
-        for i in entity[EDU_LABEL]:
+def comperevalue(entity,val):
+        
+        for i in entity['dcterms_educationLevel']:
             try:
                 pref = i.get(PREF_LABEL)
             except AttributeError:
                 continue
-            if pref == pref_value:
-                entities.append(entity)
-                break
+            if pref == val:
+                return True
+            return False
+            
+def select_entities(doc, pref_value):
+    entities = []
+    for entity in doc:
+        
+        if comperevalue(entity, pref_value):
+            entities.append(entity)
     return entities
 
 if __name__ == '__main__':
     doc = get_doc()
-    e = select_entities(doc, "5")
-    for item in e:
-        for i in  item['children']:
-            print i['asn_statementNotation'], i['id']
-        
+    e = select_entities(doc, gradefilter)
+    for elem in e:
+        print elem['id'],elem['text']
